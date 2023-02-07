675009a09a3f31831f7b51035f3413b7e1854168
sr <7518647+wendychaung@users.noreply.github.com>
2023-02-07 16:11:00 +0800

    update new footer

--- src/components/footerBox.tsx
+++ src/components/footerBox.tsx
@@ -1,9 +1,15 @@
 import styled from "styled-components";
 import {Container} from 'react-bootstrap';
+import GithubImg from "../assets/github.png";
+import TwitterImg from "../assets/Twitter.png";
+
 const Footer = styled.div`
   height: 80px;
   margin-top: 60px;
   width: 100%;
+  padding-top: 15px;
+  background: #fff;
+  font-size: 14px;
   .midBox{
     display: flex;
     justify-content: space-between;
@@ -16,18 +22,28 @@ const Footer = styled.div`
   .lft{
     padding-left: 10px;
   }
+  a{
+    display: inline-block;
+    margin-left: 20px;
+  }
 `
 export default function footerBox(){
     return  <Footer>
         <Container>
             <div className="midBox">
-                <div className="lft">&copy; 2022 Web3camp.us</div>
+                <a href="https://web3camp.us" target="_blank" rel="noreferrer">
+                    <div className="lft">&copy; 2022 Web3camp.us</div>
+                </a>
+
                 <div>
                     <a href="https://github.com/Web3Camp-Labs/ethers-tool" target="_blank" rel="noreferrer">
-                        <img src="https://web3camp.us/assets/images/GitHub-Mark.png" alt=""/>
+                        <img src={GithubImg} alt=""/>
+                    </a>
+                    <a href="https://twitter.com/Web3Camp" target="_blank" rel="noreferrer">
+                        <img src={TwitterImg} alt=""/>
                     </a>
                 </div>
             </div>
         </Container>
-</Footer>
+    </Footer>
 }
